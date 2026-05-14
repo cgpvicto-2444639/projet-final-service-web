a84d93bbaea118bead972e9cfd346b5a9a66b23d
chris-l2 <2444639@etudiant.cegepvicto.ca>
2026-05-14 16:48:23 -0400

    Comit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import fs from 'fs';
 const swaggerDocument = JSON.parse(fs.readFileSync('./src/config/documentation.json', 'utf8'));
 import router from './src/routes/livres.route.js'
 import cors from 'cors';
+import morgan from 'morgan';
 
 const swaggerOptions = {
     customCss: '.swagger-ui .topbar { display: none }',
@@ -16,8 +17,14 @@ dotenv.config();
 
 // Middleware
 app.use(express.json());
-
 app.use(cors());
+
+// Morgan
+const errorLogStream = fs.createWriteStream('./errors.log', { flags: 'a' });
+app.use(morgan('combined', {
+  stream: errorLogStream,
+  skip: (req, res) => res.statusCode !== 500
+}));
 
 // Visuel documentation
 app.use('/api/docs',
